fb174743d7ba6018d2d24c2d6175d7d090aa6248
Evan Czaplicki <info@elm-lang.org>
2014-07-07 12:21:41 +0200

    Make TodoMVC build easier

--- make.sh
+++ make.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
 mkdir -p Native
 browserify wrapper.js -o Native/Html.js
 echo ";" >> Native/Html.js
+elm --make examples/todo/Todo.elm
+mv build/examples/todo/Todo.html examples/todo/index.html
+rm -rf build
