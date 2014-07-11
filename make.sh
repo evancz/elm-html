273466f1afc87ab10e95f45c122228632b691e0a
Evan Czaplicki <info@elm-lang.org>
2014-07-11 16:10:01 +0200

    Bundle runtime so app can be zipped up easily

--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@
 mkdir -p Native
 browserify wrapper.js -o Native/Html.js
 echo ";" >> Native/Html.js
-elm --make examples/todo/Todo.elm
+elm --make --bundle-runtime examples/todo/Todo.elm
 mv build/examples/todo/Todo.html examples/todo/index.html
 rm -rf build
