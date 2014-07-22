f2318039b25cb240e38984a521f306c4496c8796
Evan Czaplicki <info@elm-lang.org>
2014-07-22 23:00:17 +0200

    Stop compiling the Todo app. It does not exist any more

--- make.sh
+++ make.sh
@@ -3,6 +3,3 @@
 mkdir -p Native
 browserify wrapper.js -o Native/Html.js
 echo ";" >> Native/Html.js
-elm --make --bundle-runtime --only-js examples/todo/Todo.elm
-mv build/examples/todo/Todo.js examples/todo/todo.js
-rm -rf build
