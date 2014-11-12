dc74fe53aed406557224dd2677d37dee0767963c
Evan Czaplicki <info@elm-lang.org>
2014-11-11 18:44:47 -0800

    Clean up the API for "lazy" optimizations

--- src/Html/Lazy.elm
+++ src/Html/Lazy.elm
@@ -1,12 +1,12 @@
-module Html.Optimize.RefEq where
+module Html.Lazy where
 {-| Since all Elm functions are pure we have a guarantee that the same input
 will always result in the same output. This module gives us tools to be lazy
-about building Html that utilize this fact.
+about building `Html` that utilize this fact.
 
 Rather than immediately applying functions to their arguments, the `lazy`
 functions just bundle the function and arguments up for later. When diffing
-the old and new virtual DOM, it checks to see if all the arguments are
-**referentially** equal. If so, it skips calling the function!
+the old and new virtual DOM, it checks to see if all the arguments are equal.
+If so, it skips calling the function!
 
 This is a really cheap test and often makes things a lot faster, but definitely
 benchmark to be sure!
@@ -15,14 +15,17 @@ benchmark to be sure!
 -}
 
 import Html (Html)
-import Native.Html
+import VirtualDom
+
 
 lazy : (a -> Html) -> a -> Html
-lazy = Native.Html.lazyRef
+lazy =
+    VirtualDom.lazyRef
 
 lazy2 : (a -> b -> Html) -> a -> b -> Html
-lazy2 = Native.Html.lazyRef2
+lazy2 =
+    VirtualDom.lazyRef2
 
 lazy3 : (a -> b -> c -> Html) -> a -> b -> c -> Html
-lazy3 = Native.Html.lazyRef3
-
+lazy3 =
+    VirtualDom.lazyRef3
