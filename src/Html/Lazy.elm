4a54de798d20e8df567f364932f534013603e271
Evan Czaplicki <info@elm-lang.org>
2016-03-01 12:12:36 -0800

    Update Html.Lazy for new virtual-dom

--- src/Html/Lazy.elm
+++ src/Html/Lazy.elm
@@ -1,4 +1,7 @@
-module Html.Lazy where
+module Html.Lazy
+  ( lazy, lazy2, lazy3
+  )
+  where
 {-| Since all Elm functions are pure we have a guarantee that the same input
 will always result in the same output. This module gives us tools to be lazy
 about building `Html` that utilize this fact.
@@ -26,20 +29,20 @@ can check to see if `model` is referentially equal to the previous value used,
 and if so, we just stop. No need to build up the tree structure and diff it,
 we know if the input to `view` is the same, the output must be the same!
 -}
-lazy : (a -> Html) -> a -> Html
+lazy : (a -> Html msg) -> a -> Html msg
 lazy =
   VirtualDom.lazy
 
 
 {-| Same as `lazy` but checks on two arguments.
 -}
-lazy2 : (a -> b -> Html) -> a -> b -> Html
+lazy2 : (a -> b -> Html msg) -> a -> b -> Html msg
 lazy2 =
   VirtualDom.lazy2
 
 
 {-| Same as `lazy` but checks on three arguments.
 -}
-lazy3 : (a -> b -> c -> Html) -> a -> b -> c -> Html
+lazy3 : (a -> b -> c -> Html msg) -> a -> b -> c -> Html msg
 lazy3 =
   VirtualDom.lazy3
